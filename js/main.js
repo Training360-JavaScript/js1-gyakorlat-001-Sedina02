4ce5662e143355e6d574f614ddc72082d3628297
Sedina02 <sipos.edina02@gmail.com>
2022-08-09 19:11:04 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 'use strict';
 
 
-var elements = document.querySelectorAll(".button");
+let elements = document.querySelectorAll(".button");
 
-function handleClick() {
+const handleClick = () => {
     for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener('click', () => console.log(elements[i].textContent));
     }
 }
-
-handleClick();
